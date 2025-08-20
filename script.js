a5ad2fdc3c874a5a7e7f005511e235cc57cf9c16
maryoma497 <maryoo14medo15@gmail.com>
2025-08-21 01:34:58 +0300

    Update
    added a name, age, and a city. added a console.log for 3 of them "and made the color appear green in console. letting the user entering name and age then alerting a msg displaying them

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Mariam";
+let age = 19;
+const city = "Cairo";
+console.log("%c" + (name + ", " + age + ", " + city), "color: green;");
+
+let userName = prompt("Enter your name:");
+let userAge = prompt("Enter your age:");
+alert("Hello "+userName + ", your age is " + age );
